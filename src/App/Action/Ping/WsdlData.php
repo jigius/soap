b44c978b228013216fe04725ce5411c869d880f1
jigius <jigius@gmail.com>
2021-08-17 19:31:39 +0700

    add a licinse into files

--- src/App/Action/Ping/WsdlData.php
+++ src/App/Action/Ping/WsdlData.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * This file is part of the jigius/soap library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) 2021 Jigius <jigius@gmail.com>
+ * @link https://github.com/jigius/soap GitHub
+ */
+
 declare(strict_types=1);
 
 namespace Jigius\Soap\App\Action\Ping;
